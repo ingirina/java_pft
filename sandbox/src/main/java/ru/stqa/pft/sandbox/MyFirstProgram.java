15d35d7a4b7f16244096586d02381e3f85289b47
ingirina <inga.minkevich@gmail.com>
2017-10-24 13:01:13 -0700

    Added method distance(Point p) to class Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,22 +4,17 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(-1, 3);
-    Point p2 = new Point(2, 3);
+    Point p1 = new Point(1, 5);
+    Point p2 = new Point(2, 11);
 
     System.out.println("Distance between point A(" + p1.x + "," + p1.y + ") and point B(" + p2.x + "," + p2.y + ") equals " + distance(p1, p2));
 
+    System.out.println("Distance between point A(" + p1.x + "," + p1.y + ") and point B(" + p2.x + "," + p2.y + ") equals " + p1.distance(p2));
+
   }
-  
 
   public static double distance(Point p1, Point p2) {
     return Math.sqrt(Math.pow((p1.x-p2.x),2)+Math.pow((p1.y-p2.y),2));
   }
-
-
-
-
-
-
 
 }
