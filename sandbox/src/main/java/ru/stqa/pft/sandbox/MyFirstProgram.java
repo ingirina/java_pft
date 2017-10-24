e1c8c3d71383a0b54f56419d9c1b3a527ce66c6e
ingirina <inga.minkevich@gmail.com>
2017-10-24 09:49:34 -0700

    Added some methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("World");
+    hello("Inga");
+    hello("user");
+
+    double len = 6;
+    System.out.println("Площадь квадрата со стороной " + len + " равна " + area(len));
+
+    double a = 3;
+    double b = 5;
+    System.out.println("Площадь прямоугоульника со сторонами " + a + " и " + b + " равна " + area(a, b));
+
   }
+
+  public static void hello (String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double l) {
+    return l*l;
+  }
+
+  public static double area (double a, double b) {
+    return a*b;
+  }
+
 
 }
