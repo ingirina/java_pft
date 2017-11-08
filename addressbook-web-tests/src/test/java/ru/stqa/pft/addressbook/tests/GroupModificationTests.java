c8e42115effbac9be1705eb99a02288c70b2fbe5
ingirina <inga.minkevich@gmail.com>
2017-11-08 13:47:08 -0800

    Optimized the filling of text fields

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("testEdit1", "testEdit2", "testEdit3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "testEdit2", "testEdit3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
